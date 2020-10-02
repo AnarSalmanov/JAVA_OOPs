4caf63944fac03d9231f13feae606479bffcbd6f
Anar <salmanovanar@gmail.com>
2020-10-02 00:32:35 -0700

    added Object class

---
+++ src/Object_class/Object_class_intro.java
@@ -0,0 +1,10 @@
+package Object_class;
+
+public class Object_class_intro {
+
+    /** ---------------  Object class  ----------------------
+     * Object class is the base class for all other classes
+     * All classes extends Object class and inherits methods from them.
+     --> for example: toString();
+     */
+}
